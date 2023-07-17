8763983f2422e4ba35b013d059564b6864ed585d
Bella Ball <mohawkball@gmail.com>
2023-07-17 10:51:38 -0500

    'added print statement'

--- customer_country.py
+++ customer_country.py
@@ -5,9 +5,14 @@ customer_file = csv.reader(customers, delimiter=',')
 
 country_file = open('customer_country.csv','w')
 
+counter = 0
+
 next(customer_file)
 for record in customer_file:
     country_file.write(record[1]+ ' ' + record[2] + ' ' + record[4] + '\n')
+    counter += 1
+
+print("Total Customers: " + str(counter))
 
 country_file.close()
 customers.close()
